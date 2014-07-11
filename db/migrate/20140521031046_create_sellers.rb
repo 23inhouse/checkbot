49b47223100622b295228dfa5993e0596652a7d0
Benjamin Lewis <23inhouse@gmail.com>
2014-07-11 14:52:49 +0930

    remove the rails migrations

--- db/migrate/20140521031046_create_sellers.rb
+++
@@ -1,9 +0,0 @@
-class CreateSellers < ActiveRecord::Migration
-  def change
-    create_table :sellers do |t|
-      t.decimal :handling_charges
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140521034628_create_winelists.rb
+++
@@ -1,9 +0,0 @@
-class CreateWinelists < ActiveRecord::Migration
-  def change
-    create_table :winelists do |t|
-      t.references :seller, index: true
-
-      t.timestamps
-    end
-  end
-end
